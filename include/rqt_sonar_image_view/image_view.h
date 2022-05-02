5354e703f4e6c52f4fe82dfc2095dd3de3f6f677
Aaron Marburg <amarburg@uw.edu>
2022-05-02 13:50:03 -0700

    Updated copyright notice in README

--- include/rqt_sonar_image_view/image_view.h
+++ include/rqt_sonar_image_view/image_view.h
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2022, Aaron Marburg,  University of Washington
+ *
+ * Based on rqt_image_view plugin:
  * Copyright (c) 2011, Dirk Thomas, TU Darmstadt
  * All rights reserved.
  *
@@ -12,9 +15,10 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the TU Darmstadt nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
+ *   * Neither the name of the TU Darmstadt or University of Washington
+ *     nor the names of its contributors may be used to endorse or
+ *     promote products derived from this software without specific
+ *     prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
